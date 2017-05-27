b7323fa0dec19c0c63f5537f422d23077a652145
Aapo Saaristo <aapo.saaristo@gmail.com>
2017-05-27 21:29:21 +0300

    initial commit

---
+++ Assets/Editor/Exporter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class Exporter : MonoBehaviour {
+
+    [MenuItem("Unity3DRIder/ExportBundles")]
+    static void ExportAsset() {
+        //BuildPipeline.BuildAssetBundles("Assets/ABs", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        AssetDatabase.ExportPackage("Assets/Plugins/Editor/JetBrains", "Assets/Rider.unitypackage", ExportPackageOptions.Recurse);
+    }
+
+    static Exporter() { ExportAsset(); }
+
+}
